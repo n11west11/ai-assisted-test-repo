6ab73e3c0742b4fae769a55cafa9b4a1a133f831
n11west11 <n11west11@gmail.com>
2023-12-15 12:12:31 -0700

    fix: adjust package versions for Python 3.10 compatibility
    Updated the dependencies to be compatible with Python version 3.10 while ensuring that they meet the required functionality and there are no conflicts.
    
    Changes made:
    - Python version constraint updated to ">=3.10,<3.12".
    - Updated `openai` from "^1.3.9" to "1.4.0".
    - Downgraded `fastapi` from "^0.105.0" to "0.100.1" which is the latest compatible version before "0.105.0".
    - Downgraded `uvicorn` from "^0.24.0.0.post1" to "0.23.2" which is the latest compatible version before "0.24.0".
    - Added `chainlit` version "^0.7.700" as a new dependency, assuming it's necessary for your project based on the diff provided.
    - Removed unnecessary extras from uvicorn since they were causing incompatibilities or are not needed.
    - Adjusted sub-dependencies to match the main dependencies' downgrade for compatibility.
    
    Please ensure that all functionalities are properly tested after these changes to validate compatibility.

---
+++ ai_assisted_test_repo/tools/export_model.py
@@ -0,0 +1,25 @@
+import json
+import inspect
+import os
+
+from pydantic import BaseModel
+
+
+def export_model(model: BaseModel):
+
+    caller_frame = inspect.stack()[1]
+    caller_path = caller_frame.filename
+    caller_dir = os.path.dirname(os.path.abspath(caller_path))
+
+    output_dir = os.path.join(caller_dir, "functions") 
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    file_path = os.path.join(output_dir, f"{model.__name__}.json")
+    with open(file_path, "w", encoding="utf-8") as f:
+        operation_execute = {
+            "name": f"{model.__name__}",
+            "description": model.__doc__,
+            "parameters": model.model_json_schema()
+        }
+        json.dump(operation_execute, f, indent=4)
---
+++ ai_assisted_test_repo/tools/ui/model.py
@@ -0,0 +1,91 @@
+import json
+from enum import Enum
+import os
+from typing import Optional, Dict, Any
+
+from pydantic import BaseModel, Field
+
+from ai_assisted_test_repo.tools.export_model import export_model
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_DIR = os.path.join(MODULE_DIR, "functions")
+
+
+class PlaywrightOperationType(str, Enum):
+    goto = "goto"
+    get_by_text = "get_by_text"
+    locator = "locator"
+    click = "click"
+    fill = "fill"
+    screenshot = "screenshot"
+    scroll_into_view_if_needed = "scroll_into_view_if_needed"
+    go_back = "go_back"
+    go_forward = "go_forward"
+    hover = "hover"
+
+
+class BeautifulSoupElementTypes(str, Enum):
+    div = "div"
+    button = "button"
+    input = "input"
+    select = "select"
+    textarea = "textarea"
+    audio = "audio"
+    video = "video"
+    embed = "embed"
+    iframe = "iframe"
+    img = "img"
+    object = "object"
+    canvas = "canvas"
+    map = "map"
+    meter = "meter"
+    progress = "progress"
+    svg = "svg"
+    template = "template"
+    details = "details"
+    dialog = "dialog"
+    menu = "menu"
+    summary = "summary"
+    span = "span"
+    a = "a"
+    p = "p"
+    h1 = "h1"
+    h2 = "h2"
+    h3 = "h3"
+    h4 = "h4"
+    h5 = "h5"
+    h6 = "h6"
+
+
+class PageContent(BaseModel):
+    """
+     Model representing the content of a webpage.
+    """
+    items_to_find: BeautifulSoupElementTypes = Field(description="type of element to search for")
+    text_to_find: Optional[str] = Field(default="", description="text to find in the page content")
+
+    class Config:
+        use_enum_values = True
+
+
+class ExecutePlaywrightOperation(BaseModel):
+    """
+    Model representing an operation to be executed using Playwright.
+    """
+    operation: PlaywrightOperationType = Field(description="playwright operation to execute")
+    args: list = Field(default_factory=list, description="positional arguments for a page operation")
+    kwargs: Optional[Dict[str, Any]] = Field(default_factory=dict, description="keyword arguments for a page operation")
+
+
+    class Config:
+        use_enum_values = True
+
+
+if __name__ == "__main__":
+    # export models to json
+
+    for model in [
+        ExecutePlaywrightOperation,
+        PageContent,
+    ]:
+        export_model(model)
