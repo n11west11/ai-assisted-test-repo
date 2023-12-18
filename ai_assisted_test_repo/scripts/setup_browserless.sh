b9c4c20e8fc5fb4215549322b7ec371a11181233
Nathan <82956888+n11west11@users.noreply.github.com>
2023-12-18 13:47:25 -0700

    Update setup_browserless.sh

--- ai_assisted_test_repo/scripts/setup_browserless.sh
+++ ai_assisted_test_repo/scripts/setup_browserless.sh
@@ -1,1 +1,1 @@
-docker run -p 3000:3000 -p 5900:5900 --name browserless -e "DEBUG=true" -e "ENABLE_DEBUGGER=true" -e "MAX_CONCURRENT_SESSIONS=10" -e “CONNECTION_TIMEOUT=5400000” -e “PWDEBUG=1” browserless/chrome
+docker run -p 3000:3000 -p 5900:5900 --name browserless -e "DEBUG=true" -e "ENABLE_DEBUGGER=true" -e "MAX_CONCURRENT_SESSIONS=10" -e CONNECTION_TIMEOUT=5400000 -e PWDEBUG=1 browserless/chrome
